f7cfc01423e86d1e259afeda42d94e52652821c0
Hazael Lopez <elhazael134@gmail.com>
2020-09-09 23:20:33 -0600

    Creamos la configuracion en DATABASES de settings.py para hacer nuestra migracion de sqlite3 a mysql

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'biblioteca',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOsT': 'localhost',
+        'PORT': 3306,
     }
 }
 
