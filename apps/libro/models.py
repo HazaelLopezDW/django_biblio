5bde08837091bc564aadb95aa3e418f435236db3
Hazael Lopez <elhazael134@gmail.com>
2020-09-10 22:25:43 -0600

    Arreglamos el artributo CharField que teniamos malo en titulo

--- apps/libro/models.py
+++ apps/libro/models.py
@@ -18,7 +18,7 @@ class Autor(models.Model):
 
 class Libro(models.Model):
     id = models.AutoField(primary_key = True)
-    titulo = models.charField('Titulo', max_length, blank = False, null = False)
+    titulo = models.CharField('Titulo', max_length, blank = False, null = False)
     fecha_publicacion = models.DateField('Fecha de publicacion', blank = False, null = False)
     autor_id = models.OneToOneField(Autor)
 
