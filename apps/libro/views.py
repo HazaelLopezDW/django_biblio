4268bdff8e6a8886cfee916ac2fc2776f8cb20cf
Hazael Lopez <elhazael134@gmail.com>
2020-09-14 21:57:41 -0600

    Ternamos de nuestra funcion crearAutor(request):

--- apps/libro/views.py
+++ apps/libro/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 def Home(request):
     return render(request, 'index.html')
 
 def crearAutor(request):
-    if request.method = 'POST':
-        
+    if request.method == 'POST':
+        autor_form = AutorForm(request.POST)
+        if autor_form.is_valid():
+            autor_form.save()
+            return redirect('index')
+    else:
+        autor_form = AutorForm()
+    return redirect(request,'libro/crear_autor.html', {'autor_form':autor_form})
