b4ba2ed12c177288069377cab7d1dffc5214a313
Hazael Lopez <elhazael134@gmail.com>
2020-09-29 11:24:49 -0600

    Estamos viendo nuestra vistas basadas en clases

--- apps/libro/views.py
+++ apps/libro/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render,redirect
 from django.core.exceptions import ObjectDoesNotExist
 from .forms import AutorForm
 from .models import Autor
+from django.views.generic import TemplateView,ListView
 
-def Home(request):
-    return render(request, 'index.html')
+"""
+    1.- dispach: Valida la peticion y elige que metodo HTTP se utlizo para la solicitud
+    2.- http_method_not_allowed(): retorna un error cuando se utiliza un metodo HTTP no soportado o definido
+    3.- options()
+"""
 
-def crearAutor(request):
+class Inicio(TemplateView):
+    template_name = 'index.html'
+
+class ListadoAutor(ListView):
+    model = Autor
+    template_name = 'libro/listar_autor.html'
+    context_object_name = 'autores'
+    queryset = Autor.objects.filter(estado = True)
+
+def CrearAutor(request):
     if request.method == 'POST':
         autor_form = AutorForm(request.POST)
         if autor_form.is_valid():
@@ -16,11 +29,13 @@ def crearAutor(request):
         autor_form = AutorForm()
     return render(request,'libro/crear_autor.html',{'autor_form':autor_form})
 
-def listarAutor(request):
+
+
+def ListarAutor(request):
     autores = Autor.objects.filter(estado = True)
     return render(request, 'libro/listar_autor.html',{'autores':autores})
 
-def editarAutor(request,id):
+def EditarAutor(request,id):
     autor_form = None
     error = None
     try:
@@ -37,7 +52,7 @@ def editarAutor(request,id):
 
     return render(request,'libro/crear_autor.html', {'autor_form':autor_form, 'error':error})
 
-def eliminarAutor(request,id):
+def EliminarAutor(request,id):
     autor = Autor.objects.get(id = id)
     if request.method == 'POST':
         autor.estado = False
