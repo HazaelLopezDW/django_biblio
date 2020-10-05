878be7b1ab081daff41fbfe8f4cb3d44b2cd825e
Hazael Lopez <elhazael134@gmail.com>
2020-10-05 11:00:02 -0600

    Agregamos class AutorAdmin(admin.ModelAdmin): y agregamos nuestro search_fields y nuestro list_display

--- apps/libro/admin.py
+++ apps/libro/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Autor,Libro
 
-admin.site.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    search_fields = ['nombre','apellidos']
+    list_display = ('nombre', 'apellidos', 'nacionalidad', 'estado', 'fecha_creacion',)
+
+admin.site.register(Autor, AutorAdmin)
 admin.site.register(Libro)
